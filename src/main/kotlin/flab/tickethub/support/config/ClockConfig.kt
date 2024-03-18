9a38e9bc5b46e88e4d3b08b0e434175b7da76223
jaeyeopme <jaeyeopme@gmail.com>
2024-03-18 15:19:37 +0900

    Feat: TokenProvider 구현
    Test: 토큰 생성 및 검증 케이스 작성

---
+++ src/main/kotlin/flab/tickethub/support/config/ClockConfig.kt
@@ -0,0 +1,13 @@
+package flab.tickethub.support.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.Clock
+
+@Configuration
+class ClockConfig {
+
+    @Bean
+    fun clock(): Clock = Clock.systemDefaultZone()
+
+}
